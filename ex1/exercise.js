7b53eb18d1beb2ce9fba861ec24a398d55711977
amie senghore <amiesenghore1@gmail.com>
2021-03-04 13:03:51 +0100

    ex1

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,14 @@
 // define your functions here
 
 // call the functions and connect them to an event
+
+var strong = document.querySelectorAll ("strong");
+var link = document.querySelector("a");
+link.addEventListener("mouseover" ,changecolor);
+
+function changecolor(){
+    strong.forEach(function(item){
+        item.style.backgroundColor = "yellow";
+    })
+
+}
